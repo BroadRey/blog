efc35d7293d3dca4c14f1ec3896f435c58b1c7ca
andrey <larginator@gmail.com>
2023-06-03 23:52:27 +0300

    ✨ Add hello, now_date and goodby views

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from posts.views import hello_view, now_date_view, goodby_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', hello_view),
+    path('now_date/', now_date_view),
+    path('goodby/', goodby_view),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,19 @@
+from datetime import date
+
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def hello_view(request: HttpRequest) -> HttpResponse:
+    if request.method == 'GET':
+        return HttpResponse('Hello! Its my project')
+
+
+def now_date_view(request: HttpRequest) -> HttpResponse:
+    if request.method == 'GET':
+        return HttpResponse(date.today())
+
+
+def goodby_view(request: HttpRequest) -> HttpResponse:
+    if request.method == 'GET':
+        return HttpResponse('Goodby user!')
