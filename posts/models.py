2a6a94ff7eae7110ca6356816e93a019f3d5503a
andrey <larginator@gmail.com>
2023-06-04 17:23:56 +0300

    💾 Add Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
+from pyexpat import model
+from turtle import mode
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    image = models.ImageField()
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    rate = models.FloatField(default=0)
+    created_at = models.DateTimeField(auto_now=True)
+    modified_at = models.DateTimeField(auto_now_add=True)
