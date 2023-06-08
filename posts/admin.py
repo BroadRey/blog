b89b473f18f9136d33c979ef68268f67e3ba398f
andrey <larginator@gmail.com>
2023-06-08 20:14:03 +0300

    💾 Add Hashtag model and m2m with Post

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from posts.models import Post
+from posts.models import Hashtag, Post
 
 admin.site.register(Post)
+admin.site.register(Hashtag)
--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,10 @@ from turtle import mode
 from django.db import models
 
 
+class Hashtag(models.Model):
+    title = models.CharField(max_length=36)
+
+
 class Post(models.Model):
     image = models.ImageField()
     title = models.CharField(max_length=256)
@@ -10,3 +14,8 @@ class Post(models.Model):
     rate = models.FloatField(default=0)
     created_at = models.DateTimeField(auto_now=True)
     modified_at = models.DateTimeField(auto_now_add=True)
+    hashtags = models.ManyToManyField(Hashtag)
+
+    @property
+    def hashtags_list(self):
+        return self.hashtags.all()
